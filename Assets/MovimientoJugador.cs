ea1ba987cc1cb553f96d543adcad664ff212584e
Pablo Cardenas A <pablo.cardenas30@inacapmail.cl>
2023-11-22 09:42:42 -0300

    primer commit

---
+++ Assets/MovimientoJugador.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoJugador : MonoBehaviour
+{
+    [SerializeField]private float speed = 5f;
+    private Rigidbody2D playerRb;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        moveInput = new Vector2(moveX, moveY).normalized;
+    }
+    private void FixedUpdate()
+    {
+        playerRb.MovePosition(playerRb.position + moveInput * speed * Time.fixedDeltaTime);
+    }
+}
